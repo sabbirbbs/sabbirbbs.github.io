37bb2f19f88a44a0eaea7833c0f4fb2ff4364e53
MD Sabbir Hosen <47492470+sabbirbbs@users.noreply.github.com>
2020-07-24 10:57:13 -0400

    Add files via upload

---
+++ assets/blogScript.js
@@ -0,0 +1,15 @@
+var posts = [];
+
+function blog(){
+document.getElementById("content").innerHTML = blogPage;
+if(posts === undefined || posts.length == 0){
+	document.getElementById("postLink").innerHTML = ` No note has been published yet.`;
+}//If not post has been published.
+else{
+for(var i = 0;i<posts.length;i++){
+var noteNum = Math.abs(i-posts.length);
+var oc =`onclick="note('notes/`+noteNum+`.html','`+noteNum+`')"`;
+document.getElementById("postLink").innerHTML += '<a name="'+noteNum+'" href="#'+posts[i]+'" '+oc+'>'+'<span style="color:white">BBS-Note-'+noteNum+'</span>> '+posts[i]+'</a>'+'<br>';
+}
+}
+}//Entire block under the function
