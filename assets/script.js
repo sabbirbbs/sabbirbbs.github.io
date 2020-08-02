9a7070eb3edefa5a2f46e628a4e7d1e74a2053fd
MD Sabbir Hosen <47492470+sabbirbbs@users.noreply.github.com>
2020-08-02 10:44:24 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -47,7 +47,7 @@ const contactPage = `<div id="root"><span style="color:rgb(52, 235, 225);">bbs@d
 
 const blogPage = `<div id="root"><span>root</span>@devil:/var/www/html/notes# apt-get upgrade</div>
 			<span>Get:1 <a href="#project-1" onclick="project('notes/1.html')">Demo project</a></span><span style="float:right;padding-right:0%;"> from projects</span></br>
-			<span>Get:2 <a href="#note-1" onclick="note('notes/1.html','1')">Latest post 1</a></span><span style="float:right;padding-right:0%;"> from notes</span></br>
+			<span>Get:2 <a href="#note-1" onclick="note('notes/1.html','1')">Mad libs game</a></span><span style="float:right;padding-right:0%;"> from notes</span></br>
 			<span>Get:3 <a href="#note-1" onclick="note('notes/1.html','1')">Latest post 2</a></span><span style="float:right;padding-right:0%;">  from notes</span></br>
 			<span>Fetching latest projects... Done
 			<div id="root"><span>root</span>@devil:/var/www/html/notes# grep BBS-Note | cd notes</div>
