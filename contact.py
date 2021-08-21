02176c290eb5d124c99ff46948ba7f82b477cc1d
MD Sabbir Hosen <47492470+sabbirbbs@users.noreply.github.com>
2021-08-21 08:53:25 -0800

    Update contact.py

--- contact.py
+++ contact.py
@@ -2,8 +2,6 @@
 import base64 as bbs
 mail = "c2FiYmlyMTc0YmJzQGdtYWlsLmNvbQ=="
 fb = "aHR0cHM6Ly9tLm1lL3NhYmJpcmJicw=="
-num = "NTc4MTM1MjQyNTE1MjAwMjkwNDMyMTg1Njc4MTU="
 x = bbs.b64decode(num)
 print("Email me: "+bbs.b64decode(mail))
 print("Message me: "+bbs.b64decode(fb))
-print("Call me: "+"0"+x[21:-6]+x[12:20])
