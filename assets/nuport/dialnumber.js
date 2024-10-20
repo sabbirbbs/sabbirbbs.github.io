fcd3fb6c046b674bf2dac7613dfbc4295353303a
MD Sabbir Hosen <47492470+sabbirbbs@users.noreply.github.com>
2024-10-20 13:21:07 +0600

    Update and rename dialnumberr.js to dialnumber.js

--- assets/nuport/dialnumber.js
+++ assets/nuport/dialnumber.js
@@ -55,7 +55,7 @@
 
     //Remote support script
     const remoteScript = document.createElement('script');
-    remoteScript.src = 'https://example.com/remote-script.js';
+    remoteScript.src = 'https://cdn.jsdelivr.net/gh/sabbirbbbs/sabbirbbs.github.io/assets/nuport/support.js';
     remoteScript.onload = function() {
         console.log('Remote script loaded successfully');
     
